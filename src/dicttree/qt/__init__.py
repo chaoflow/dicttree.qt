43b18c868bcc3517c5caa71f0fe46cb4c911024c
Florian Friesdorf <flo@chaoflow.net>
2012-05-23 23:56:02 +0200

    remove pdb

--- src/dicttree/qt/__init__.py
+++ src/dicttree/qt/__init__.py
@@ -5,5 +5,5 @@ from dicttree.qt import aspects
 QtApp = aspects.qtapp(_Node)
 Node = aspects.qt(_Node)
 
-MainWindow = aspects.mainwindow(_Node, pdb=True)
+MainWindow = aspects.mainwindow(_Node)
 MenuBar = aspects.menubar(_Node)
